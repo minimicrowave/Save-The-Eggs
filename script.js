a541a48fe3552bbc01aeec88fe76f42f7844a83d
Serene Lim <Serene@serenes-mac.local>
2018-12-10 05:39:48 +0800

    added duckies mobbing at different intervals

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 var chick;
 var theObstacles;
+var duckies = [];
 var score;
 
 function startGame() {
     gameArea.start();
     chick = new component(30, 30, "yellow", 550, 350);
+    // chick = new component(30, 30, "chick.png", 550, 350, "image");
+    theObstacles = new component(20, 20, "black", 0, 330);
     // theObstacles = new component(20, 20, "ducky.png", 20, 300, "image");
     score = new component("20px", "BenchNine", "black", 20, 30, "text");
     // eggs = new component(30, 30, "egg.png", );
     window.addEventListener('keydown', doKeyDown, true);
     // window.addEventListener('keyup', doKeyUp, true);
+
+
+
 }
 
 function component(width, height, color, x, y, type) {
@@ -30,7 +36,7 @@ function component(width, height, color, x, y, type) {
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
             ctx.fillText(this.text, this.x, this.y);
-        } else if (type == "image") {
+        } else if (this.type == "image") {
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
         } else {
             ctx.fillStyle = color;
@@ -74,36 +80,53 @@ function component(width, height, color, x, y, type) {
         if (this.x + this.width > gameArea.canvas.width || this.x < 0) {
             this.speedX = -this.speedX;
         }
-        if (this.y + this.height > gameArea.canvas.height || this.y < 0) {
+        if (this.y + this.height > gameArea.canvas.height || this.y < 40) {
             this.speedY = -this.speedY;
         }
     }
+    this.reset = function () {
+        for (var i in duckies){
+            if (duckies[i].x > gameArea.canvas.width){
+                duckies[i].x = -40;
+                duckies[i].y = (Math.random() * 530) + 40;
+                duckies[i].x += Math.random() * 5;
+            }
+        }
+    }
 }
+
+function everyinterval(n) {
+    if ((gameArea.frameNo / n) % 1 == 0) {
+        return true;
+    }
+    return false;
+}
+
 
 // Keys Functions
 function doKeyDown(evt) {
     switch (evt.keyCode) {
         case 38:
             /* Up arrow is pressed */
-            if (chick.y - 2 > 0 && chick.speedY > -5) {
+            if (chick.y - 2 > 0 && chick.speedY > -3) {
                 chick.speedY -= 1;
             }
             break;
         case 40:
             /* Down arrow is pressed */
-            if (chick.y + 2 < gameArea.canvas.height && chick.speedY < 5) {
+            if (chick.y + 2 < gameArea.canvas.height && chick.speedY < 3) {
                 chick.speedY += 1;
             }
             break;
         case 37:
             /* Left arrow is pressed */
-            if (chick.x - 2 > 0 && chick.speedX > -5) {
+            if (chick.x - 2 > 0 && chick.speedX > -3) {
                 chick.speedX -= 1;
             }
             break;
         case 39:
             /* Right arrow is pressed */
-            if (chick.x + 2 < gameArea.canvas.width && chick.speedX < 5) {
+            if (chick.x + 2 < gameArea.canvas.width && chick.speedX < 3) {
                 chick.speedX += 1;
             }
             break;
@@ -121,6 +144,7 @@ var gameArea = {
         this.canvas.width = 600;
         this.context = this.canvas.getContext("2d");
         document.querySelector("main").appendChild(this.canvas);
+        this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 40);
     },
 
@@ -136,137 +160,79 @@ var gameArea = {
 // Update Game Area
 
 function updateGameArea() {
-    // if (chick.crashWith(theObstacles)) {
-    //     gameArea.stop();
-    // } else {
-        gameArea.clear();
+    var x, y;
+    for (i = 0; i < duckies.length; i++) {
+        if (chick.crashWith(duckies[i])) {
+            gameArea.stop();
+            return;
+        }
+    }
+    gameArea.clear();
+    gameArea.frameNo += 1;
+    var intervals = Math.random() * 100;
+    if ((gameArea.frameNo == 1) || everyinterval(70)) {
+        duckies.push(new component(20, 20, "blue", (Math.random() * 5), ((Math.random() * 530) + 40)))
+    }
+    for (i = 0; i < 6; i++) {
+        duckies[i].x += Math.random() * 5;
+        duckies[i].reset();
+        duckies[i].update();
+        duckies[i].newPos();
+
+
+
+
+        // if (chick.crashWith(theObstacles)) {
+        //     gameArea.stop();
+        // } else {
+        //     theObstacles.x += 1;
+        //     gameArea.clear();
         score.text = "Score: "
         score.update();
-        // theObstacles.update();
+        //     theObstacles.update();
         chick.newPos();
         // chick.contain();
         chick.bounce();
         chick.update();
-        // console.log("speed x ", chick.speedX);
-        // console.log("speed y ", chick.speedY);
-    // }
+        // }
+    }
 }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    // duckyImage = new Image();
+
+    // duckyImage.onload = () => {
+    //     duckyReady = true;
+    // }
+
+    // duckyImage.src = "ducky.png";
+
+    // class Ducky {
+    //     constructor(speed, x, y){
+    //         this.speed = x;
+    //         this.x = x;
+    //         this.y = y;
+    //     }
+    // }
 
 
+    // var duckyCaught = 0;
 
+    // for (var i = 0; i < 3; i++) {
+    //     duckies.push(new component(20, 20, "blue", (Math.random() * 4), ((Math.random() * 560) + 40)));
+    // }
 
+    // function reset(m) {
+    //     duckies[m].x = -40;
+    //     duckies[m].y = (Math.random() * 560) + 40;
+    //     duckies[m].speed = Math.random() * 3;
+    // }
 
-
-
-
-
-
-
-/////////////////////////////////////////////////////////////
-
-// function Drawable() {
-//     this.init = function(x, y) {
-//         // Default variables
-//         this.x = x;
-//         this.y = y;
-//     }
-//     this.speed = 0;
-//     this.canvasWidth = 0;
-//     this.canvasHeight = 0;
-//     // Define abstract function to be implemented in child objects
-//     this.draw = function() {
-//     };
-// }
-
-// var imageRepository = new function() {
-// 	// Define images
-// 	this.background = new Image();
-// 	// Set images src
-//     this.background.src = "imgs/bg.png";
-// // }
-
-
-
-
-
-// // remember to add in fn that starts this 20 second timer.
-// function update() {
-//     var element = document.getElementById("myprogressBar");
-//     var width = 100;
-//     var identity = setInterval(scene, 50);
-
-//     function scene() {
-//         if (width === 0) {
-//             clearInterval(identity);
-//         } else {
-//             width -= 0.25;
-//             element.style.width = width + '%';
-//         }
-//     }
-// }
-
-// // startGame();
-// function Game() {
-// 	/*
-// 	 * Gets canvas information and context and sets up all game
-// 	 * objects.
-// 	 * Returns true if the canvas is supported and false if it
-// 	 * is not. This is to stop the animation script from constantly
-// 	 * running on older browsers.
-// 	 */
-// 	this.init = function() {
-// 		// Get the canvas element
-// 		this.bgCanvas = document.getElementById('background');
-// 		// Test to see if canvas is supported
-// 		if (this.bgCanvas.getContext) {
-// 			this.bgContext = this.bgCanvas.getContext('2d');
-// 			// Initialize objects to contain their context and canvas
-// 			// information
-// 			Background.prototype.context = this.bgContext;
-// 			Background.prototype.canvasWidth = this.bgCanvas.width;
-// 			Background.prototype.canvasHeight = this.bgCanvas.height;
-// 			// Initialize the background object
-// 			this.background = new Background();
-// 			this.background.init(0,0); // Set draw point to 0,0
-// 			return true;
-// 		} else {
-// 			return false;
-// 		}
-// 	};
-// 	// Start the animation loop
-// 	this.start = function() {
-// 		animate();
-// 	};
-// }
-
-//////////////////////////////////////////////////////////////
-
-// function doKeyUp(evt) {
-//     switch (evt.keyCode) {
-//         case 38:
-//             /* Up arrow is released */
-//             if (chick.y - 2 > 0 && chick.speedY >= 0) {
-//                 chick.speedY += 0.025;
-//             }
-//             break;
-//         case 40:
-//             /* Down arrow is released */
-//             if (chick.y + 2 < 300 && chick.speedY >= 0) {
-//                 chick.speedY -= 0.025;
-//             }
-//             break;
-//         case 37:
-//             /* Left arrow is released */
-//             if (chick.x - 2 > 0 && chick.speedX >= 0) {
-//                 chick.speedX += 0.025;
-//             }
-//             break;
-//         case 39:
-//             /* Right arrow is released */
-//             if (chick.x + 2 < 300 && chick.speedX >= 0) {
-//                 chick.speedX -= 0.025;
-//             }
-//             break;
-//     }
-// }
+    // function update() {
+    //     for (var j in duckies){
+    //         if (monsters[j].x > canvas.width) {
+    //             reset(j);
+    //         } 
+    //     }
+    // }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
